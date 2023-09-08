b4479c1226cd9dc113eca324471853a7321ebce4
Dender46 <dmitriy.bagandov@gmail.com>
2023-09-08 18:44:29 +0300

    Fix special case for imm_reg_mem

--- src/main.cpp
+++ src/main.cpp
@@ -110,7 +110,8 @@ int main(int argc, char* argv[])
     //std::ifstream file("listings/listing_0044_register_movs", std::ios::binary);
     //std::ifstream file("listings/listing_0046_add_sub_cmp", std::ios::binary);
     //std::ifstream file("listings/listing_0048_ip_register", std::ios::binary);
-    std::ifstream file("listings/listing_0049_conditional_jumps", std::ios::binary);
+    //std::ifstream file("listings/listing_0049_conditional_jumps", std::ios::binary);
+    std::ifstream file("listings/listing_0051_memory_mov", std::ios::binary);
     if (!file.is_open())
     {
         std::cerr << "!!! Can't open file !!!\n";
@@ -127,21 +128,21 @@ int main(int argc, char* argv[])
     int byteIndex = 0;
     while (byteIndex < bytes.size())
     {
-        std::string leftOperand, rightOperand;
+        std::string instructionStr, leftOperand, rightOperand;
 
-        u8 byte = bytes[byteIndex];
-        u8 bitD = (byte & 2) >> 1;
-        u8 bitW = (byte & 1);
+        u8 instructionByte = bytes[byteIndex];
+        u8 bitD = (instructionByte & 2) >> 1;
+        u8 bitW = (instructionByte & 1);
 
-        if (auto opIndex = IsReg_Mem_Reg(byte); opIndex != OpIndex::UNDEFINED)
+        if (auto opIndex = IsReg_Mem_Reg(instructionByte); opIndex != OpIndex::UNDEFINED)
         {
-            std::cout << operationNames[opIndex];
+            instructionStr = operationNames[opIndex];
 
-            byte = bytes[++byteIndex];
+            u8 adjByte = bytes[++byteIndex];
 
-            u8 mod = (byte >> 6);
-            u8 reg = (byte & 0b00'111'000) >> 3;
-            u8 rm =  (byte & 0b00'000'111);
+            u8 mod = (adjByte >> 6);
+            u8 reg = (adjByte & 0b00'111'000) >> 3;
+            u8 rm =  (adjByte & 0b00'000'111);
             if (mod == 3) // register mode
             {
                 leftOperand  = registerNames[rm][bitW];
@@ -191,16 +192,16 @@ int main(int argc, char* argv[])
                 }
             }
         }
-        else if (IsImm_Reg_Mem(byte))
+        else if (IsImm_Reg_Mem(instructionByte))
         {
             u8 bitS = bitD;
 
-            byte = bytes[++byteIndex];
-            u8 mod     = (byte >> 6);
-            u8 opIndex = (byte & 0b00'111'000) >> 3;
-            u8 rm      = (byte & 0b00'000'111);
+            u8 adjByte = bytes[++byteIndex];
+            u8 mod     = (adjByte >> 6);
+            u8 opIndex = (adjByte & 0b00'111'000) >> 3;
+            u8 rm      = (adjByte & 0b00'000'111);
 
-            std::cout << operationNames[opIndex];
+            instructionStr = operationNames[opIndex]; // can be overwritten if doesn't fir the scehe
 
             if (mod == 3) // register mode
             {
@@ -216,19 +217,25 @@ int main(int argc, char* argv[])
             }
             else if (mod == 0) // memory mode, no displacement follows
             {
-                if (opIndex == OpIndex::MOV)
+                if (((instructionByte >> 1) << 1) == 0b1100'0110) // specific MOV instruction
                 {
-                    leftOperand = effectiveAddressesStr[rm] + ']';
-                    if (bitW == 0)
-                        rightOperand = "byte " + bytes[++byteIndex];
+                    instructionStr = operationNames[OpIndex::MOV];
+                    // SPECIAL CASE
+                    if (rm == 0b0110)
+                        leftOperand += "word [" + CombineLoAndHiToString(bytes, &byteIndex) + ']';
                     else
-                        rightOperand = "word " + CombineLoAndHiToString(bytes, &byteIndex);
+                        leftOperand += "word " + std::string(effectiveAddressesStr[rm]) + ']';
+
+                    if (bitW == 0)
+                        rightOperand = bytes[++byteIndex];
+                    else
+                        rightOperand = CombineLoAndHiToString(bytes, &byteIndex);
                 }
                 else
                 {
                     if (bitW == 1)
                     {
-                         // SPECIAL CASE
+                        // SPECIAL CASE
                         if (rm == 0b0110)
                             leftOperand = "word [" + CombineLoAndHiToString(bytes, &byteIndex) + ']';
                         else
@@ -257,13 +264,14 @@ int main(int argc, char* argv[])
             }
             else if (mod == 2) // memory mode, 16-bit displacement follows
             {
-                const auto disp = CombineLoAndHiToString(bytes, &byteIndex);
+                const u16 disp = CombineLoAndHiToWord(bytes, &byteIndex);
+                const auto dispStr = std::to_string(disp);
                 //if (bitW == 1)
                 //    std::cout << effectiveAddressesStr[rm] << " - " << std::numeric_limits<u16>::max() - disp + 1 << ']';
                 //else
                 if (opIndex == OpIndex::MOV)
                 {
-                    leftOperand = std::string(effectiveAddressesStr[rm]) + " + " + disp + ']';
+                    leftOperand = std::string(effectiveAddressesStr[rm]) + " + " + dispStr + ']';
                     if (bitW == 0)
                         rightOperand = "byte " + std::to_string(bytes[++byteIndex]);
                     else
@@ -273,7 +281,7 @@ int main(int argc, char* argv[])
                 {
                     if (bitW == 1)
                     {
-                        leftOperand = "word " + std::string(effectiveAddressesStr[rm]) + " + " + disp + ']';
+                        leftOperand = "word " + std::string(effectiveAddressesStr[rm]) + " + " + dispStr + ']';
                         if (bitS == 0)
                             rightOperand = CombineLoAndHiToString(bytes, &byteIndex);
                         else
@@ -281,17 +289,17 @@ int main(int argc, char* argv[])
                     }
                     else
                     {
-                        leftOperand = "byte " + std::string(effectiveAddressesStr[rm]) + " + " + disp + ']';
+                        leftOperand = "byte " + std::string(effectiveAddressesStr[rm]) + " + " + dispStr + ']';
                         rightOperand = std::to_string(bytes[++byteIndex]);
                     }
                 }
             }
         }
-        else if ((byte & 0b1111'0000) == 0b1011'0000) // MOV immediate to register
+        else if ((instructionByte & 0b1111'0000) == 0b1011'0000) // MOV immediate to register
         {
-            std::cout << "mov ";
-            u8 bitW = (byte & 0b0000'1000) >> 3;
-            u8 reg  = (byte & 0b0000'0111);
+            instructionStr = operationNames[OpIndex::MOV];
+            u8 bitW = (instructionByte & 0b0000'1000) >> 3;
+            u8 reg  = (instructionByte & 0b0000'0111);
 
             u16 data = bitW == 0 ? bytes[++byteIndex] : CombineLoAndHiToWord(bytes, &byteIndex);
             leftOperand  = registerNames[reg][bitW];
@@ -303,9 +311,9 @@ int main(int argc, char* argv[])
                     ExecuteOp(OpIndex::MOV, RegisterMemoryIndex(reg, bitW), data);
             }
         }
-        else if ((byte & 0b1111'1100) == 0b1010'0000) // MOV accumulator
+        else if ((instructionByte & 0b1111'1100) == 0b1010'0000) // MOV accumulator
         {
-            std::cout << "mov ";
+            instructionStr = operationNames[OpIndex::MOV];
 
             const auto data = CombineLoAndHiToString(bytes, &byteIndex);
             if (bitD == 0)
@@ -319,10 +327,10 @@ int main(int argc, char* argv[])
                 rightOperand = "ax";
             }
         }
-        else if (auto opIndex = IsImm_Accumulator(byte); opIndex != OpIndex::UNDEFINED)
+        else if (auto opIndex = IsImm_Accumulator(instructionByte); opIndex != OpIndex::UNDEFINED)
         {
-            u8 mask = 0b0011'1100;
-            std::cout << operationNames[opIndex];
+            instructionStr = operationNames[opIndex];
+
             if (bitW == 0)
             {
                 leftOperand = "al";
@@ -334,17 +342,17 @@ int main(int argc, char* argv[])
                 rightOperand = CombineLoAndHiToString(bytes, &byteIndex);
             }
         }
-        else if (IsJump(byte) || IsLoop(byte))
+        else if (IsJump(instructionByte) || IsLoop(instructionByte))
         {
-            if (IsJump(byte))
+            if (IsJump(instructionByte))
             {
-                const u8 opIndex = byte & 0b0000'1111;
-                std::cout << jumpNames[opIndex];
+                const u8 opIndex = instructionByte & 0b0000'1111;
+                instructionStr = jumpNames[opIndex];
             }
             else
             {
-                const u8 opIndex = byte & 0b0000'0011;
-                std::cout << loopNames[opIndex];
+                const u8 opIndex = instructionByte & 0b0000'0011;
+                instructionStr = loopNames[opIndex];
             }
 
             const auto disp = (s8)bytes[++byteIndex] + 2;
@@ -367,6 +375,7 @@ int main(int argc, char* argv[])
 
         byteIndex++;
 
+        std::cout << instructionStr;
         if (leftOperand != "" && rightOperand != "")
             std::cout << leftOperand << ", " << rightOperand;
         else
